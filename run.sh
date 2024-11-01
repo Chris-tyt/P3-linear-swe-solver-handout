4482bfdabdd48e88648cf1470362c6298f6a0480
Yanting Tao <chris_yt@login22.chn.perlmutter.nersc.gov>
2024-11-01 15:08:45 -0700

    move

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 # option: mpi gpu serial serial_omp basic_serial 
 # --scenario water_drop dam_break wave river
-mod=serial_omp
+mod=gpu
 scenario=water_drop
-nx=1000
-ny=1000
-niter=10000
+nx=250
+ny=250
+niter=1000
 make $mod
 ./build/${mod} --nx ${nx} --nx ${ny} --num_iter ${niter} --scenario ${scenario} --output ${mod}.out
 python utils/visualizer.py ${mod}.out ${mod}.gif
